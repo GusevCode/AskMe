ea17ad51940b4cd7b8b43f06c09ef3903cf9f7db
GusevCode <gooogle.rostov@gmail.com>
2025-04-06 00:22:53 +0300

    init

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+QUESTIONS = [
+    {
+        'title': f'title {i}',
+        'id': i,
+        'text': f'This is text for question {i}'
+    } for i in range(1, 30)
+]
+
+def index(request):
+    return render(request, 'index.html', context={'questions': QUESTIONS})
