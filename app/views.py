880c21293e7d2a513eb2360e232435313517a6e7
alexey.gusev <gooogle.rostov@gmail.com>
2025-04-06 03:08:14 +0300

    [technopark] - implemented routings

--- app/views.py
+++ app/views.py
@@ -13,3 +13,18 @@ QUESTIONS = [
 
 def index(request):
     return render(request, 'index.html', context={'questions': QUESTIONS})
+
+def ask(request):
+    return render(request, 'ask.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def single_question(request):
+    return render(request, 'single_question.html')
+
+def settings(request):
+    return render(request, 'settings.html')
--- askme_gusev/urls.py
+++ askme_gusev/urls.py
@@ -21,5 +21,10 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('ask/', views.ask, name="ask"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('question/', views.single_question, name="single_question"),
+    path('settings/', views.settings, name="settings"),
     path('admin/', admin.site.urls),
 ]
