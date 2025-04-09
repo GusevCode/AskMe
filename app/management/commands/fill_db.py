1cd86cbbcda8e959ba598237acaaa2234a123be7
GusevCode <gooogle.rostov@gmail.com>
2025-04-10 02:52:02 +0300

    [technopark] - fill_db add unique param for entities

--- app/management/commands/fill_db.py
+++ app/management/commands/fill_db.py
@@ -6,7 +6,7 @@ import random
 import os
 from django.conf import settings
 from django.db import transaction
-
+from tqdm import tqdm
 
 def get_random_avatar():
     avatar_dir = os.path.join(settings.MEDIA_ROOT, '')
@@ -25,7 +25,7 @@ def get_random_avatar():
 
 
 BOOTSTRAP_COLORS = ["primary", "secondary", "success", "danger", "warning", "info"]
-BATCH_SIZE = 2000
+BATCH_SIZE = 10000
 
 
 class Command(BaseCommand):
@@ -33,12 +33,6 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('ratio', type=int, help='Коэффициент заполнения сущностей')
-
-    def print_progress(self, current, total, entity_name):
-        if current % 100 == 0 or current == total:
-            self.stdout.write(f"Создано {current} {entity_name} из {total}", ending='\r')
-            if current == total:
-                self.stdout.write()
 
     @transaction.atomic
     def handle(self, *args, **kwargs):
@@ -57,7 +51,7 @@ class Command(BaseCommand):
         self.stdout.write(f"Создаётся {num_users} пользователей...")
         user_objs = []
         profile_objs = []
-        for i in range(1, num_users + 1):
+        for i in tqdm(range(1, num_users + 1)):
             user = User(
                 username=fake.user_name() + str(i) + str(ent_num),
                 email=fake.email(),
@@ -65,7 +59,6 @@ class Command(BaseCommand):
             ent_num += 1
             user.set_password('password')
             user_objs.append(user)
-            self.print_progress(i, num_users, "пользователей")
 
             profile_objs.append(Profile(user=user, avatar=get_random_avatar()))
             ent_num += 1
@@ -81,14 +74,13 @@ class Command(BaseCommand):
         # Создание тегов
         self.stdout.write(f"Создаётся {num_tags} тэгов...")
         tag_objs = []
-        for i in range(1, num_tags + 1):
+        for i in tqdm(range(1, num_tags + 1)):
             tag_objs.append(Tag(
                 name=fake.word() + str(i) + str(ent_num),
                 color_class=random.choice(BOOTSTRAP_COLORS),
                 description=fake.text(max_nb_chars=50)
             ))
             ent_num += 1
-            self.print_progress(i, num_tags, "тегов")
             if i % BATCH_SIZE == 0 or i == num_tags:
                 Tag.objects.bulk_create(tag_objs)
                 tag_objs = []
@@ -98,7 +90,7 @@ class Command(BaseCommand):
         self.stdout.write(f"Создаётся {num_questions} вопросов...")
         question_objs = []
         tag_relations = []
-        for i in range(1, num_questions + 1):
+        for i in tqdm(range(1, num_questions + 1)):
             question = Question(
                 author=random.choice(users),
                 title=fake.sentence(nb_words=6),
@@ -106,7 +98,6 @@ class Command(BaseCommand):
                 is_active=True
             )
             question_objs.append(question)
-            self.print_progress(i, num_questions, "вопросов")
 
             if i % BATCH_SIZE == 0 or i == num_questions:
                 created_questions = Question.objects.bulk_create(question_objs)
@@ -128,13 +119,12 @@ class Command(BaseCommand):
         # Создание ответов
         self.stdout.write(f"Создаётся {num_answers} ответов...")
         answer_objs = []
-        for i in range(1, num_answers + 1):
+        for i in tqdm(range(1, num_answers + 1)):
             answer_objs.append(Answer(
                 author=random.choice(users),
                 question=random.choice(questions),
                 content=fake.text(max_nb_chars=150)
             ))
-            self.print_progress(i, num_answers, "ответов")
             if i % BATCH_SIZE == 0 or i == num_answers:
                 Answer.objects.bulk_create(answer_objs)
                 answer_objs = []
@@ -144,7 +134,7 @@ class Command(BaseCommand):
         self.stdout.write(f"Создаётся {num_ratings} оценок...")
         ql_objs = []
         al_objs = []
-        for i in range(1, num_ratings + 1):
+        for i in tqdm(range(1, num_ratings + 1)):
             is_positive = random.choice([True, False])
             user = random.choice(users)
             if random.choice([True, False]) and questions:
@@ -159,7 +149,6 @@ class Command(BaseCommand):
                     answer=random.choice(answers),
                     is_positive=is_positive
                 ))
-            self.print_progress(i, num_ratings, "оценок")
             if i % BATCH_SIZE == 0 or i == num_ratings:
                 QuestionLike.objects.bulk_create(ql_objs, ignore_conflicts=True)
                 AnswerLike.objects.bulk_create(al_objs, ignore_conflicts=True)
